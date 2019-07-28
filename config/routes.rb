998b80e77a6cfad5d8b04d80b03d587426e0f1f0
Travis Knight <travis.a.knight@gmail.com>
2019-07-28 17:00:35 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,17 @@ Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
   # get "/photos" => "photos#index"
 
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    post "/users" => "users#create"
+
+    post "/sessions" => "sessions#create"
+
+    get "/exercises" => "exercises#index"
+
+    post "/workouts" => "workouts#create"
+
+    get "/workouts" => "workouts#index"
+    patch "/workouts/:id" => "workouts#update"
+    delete "/workouts/:id" => "workouts#destroy"
+  end
 end
