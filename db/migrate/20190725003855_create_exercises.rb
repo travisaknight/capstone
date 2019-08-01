5cc5061f3970189e5c1e46859512b6878cfb125e
Travis Knight <travis.a.knight@gmail.com>
2019-07-31 21:26:28 -0500

    Add video_url column to exercise table. Update seeds. Reseed.

--- db/migrate/20190725003855_create_exercises.rb
+++ db/migrate/20190725003855_create_exercises.rb
@@ -3,6 +3,7 @@ class CreateExercises < ActiveRecord::Migration[6.0]
     create_table :exercises do |t|
       t.string :name
       t.integer :category_id
+      t.string :video_url
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_07_25_014753) do
   create_table "exercises", force: :cascade do |t|
     t.string "name"
     t.integer "category_id"
+    t.string "video_url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
